f406f4f409cc62a1402edf47ed14e8134160e5df
takataryusei <takata-ryusei604@g.ecc.u-tokyo.ac.jp>
2025-01-21 13:21:34 +0900

    動いた

--- sketch_jan21a/sketch_jan21a.ino
+++ sketch_jan21a/sketch_jan21a.ino
@@ -2,18 +2,25 @@
 
 // ステッピングモーターの設定
 const int around_step = 200; // 1回転のステップ数
-Stepper stepper(around_step, 14, 27, 26, 25);
+Stepper stepper1(around_step, 13, 12, 14, 27);
+Stepper stepper2(around_step, 26, 25, 33, 32);
 
 // ステップ数と回転スピード
 int step = 10;
-int speed = 200;
+int speed = 50;
 
 void setup() {
   // ステッピングモーターの初期設定
-  stepper.setSpeed(speed);
+  stepper1.setSpeed(speed);
+  stepper2.setSpeed(speed);
 }
 
 void loop() {
-  // ステッピングモーターの回転
-  stepper.step(step);
+  // ステッピングモーター1の回転
+  stepper1.step(step);
+  
+  // ステッピングモーター2の回転
+  stepper2.step(step);
+  
+  //delay(100); // 0.1秒の遅延（必要に応じて調整）
 }
